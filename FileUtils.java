90a2ffa2c4cf776d9db307ee1a72512d178d67c2
about_me.md <137526723@qq.com>
2018-08-04 23:09:18 +0800

    3

--- FileUtils.java
+++ FileUtils.java
@@ -1,23 +1,42 @@
+import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 
 public class FileUtils
 {
+    static
+    {
+        FileWriter fw = null;
+        try {
+            fw = new FileWriter("information.txt",true);
+            fw.write("Name Age GPA" + "\n");
+            fw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+
 
     public static void insert(Student student) throws IOException
     {
 		// this function use to insert new student
-        FileWriter fw = new FileWriter("information.txt",true);
         String sname = student.getName();
         int sage = student.getAge();
         double sgpa = student.getGpa();
-        fw.write(sname + " " + sage + " "+ sgpa + "\n");
-        fw.close();
+
+        FileWriter fw = new FileWriter("information.txt",true);
+        BufferedWriter bw = new BufferedWriter(fw);
+        bw.write(sname + "  " + sage + "  " + sgpa + "\n");
+        bw.close();
     }
 
 	public static void delete(Student student)
     {
 		// this function use to delete one student
+
 	}
 
 //	public static Student get()
