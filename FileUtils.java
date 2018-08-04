188506e9c4b1bce6e174bc3a270fef5ce81d6654
about_me.md <137526723@qq.com>
2018-08-04 23:42:36 +0800

    4

--- FileUtils.java
+++ FileUtils.java
@@ -1,20 +1,32 @@
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 
 public class FileUtils
 {
-    static
-    {
-        FileWriter fw = null;
+    static FileReader fr = null;
+
+    static {
         try {
-            fw = new FileWriter("information.txt",true);
-            fw.write("Name Age GPA" + "\n");
-            fw.close();
-        } catch (IOException e) {
+            fr = new FileReader("information.txt");
+        } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
     }
+
+    static BufferedReader bf = new BufferedReader(fr);
+
+
+//    static
+//    {
+//
+//        FileWriter fw = null;
+//        try {
+//            fw = new FileWriter("information.txt",true);
+//            fw.write("Name Age GPA" + "\n");
+//            fw.close();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//    }
 
 
 
@@ -39,10 +51,20 @@ public class FileUtils
 
 	}
 
-//	public static Student get()
-//    {
-//		// this function ues to get one student
-//
-//	}
+	public static void /*Student*/ get() throws IOException
+    {
+		// this function ues to get one student
+
+        String line = bf.readLine();
+        String[] strs = line.split("  ");
+        for (int i=0, len=strs.length ; i<len;i++)
+        {
+            System.out.println(strs[i].toString());
+        }
+
+
+
+
+	}
 
 }
--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
@@ -13,5 +14,7 @@ public class Main{
 	public static void main(String[] args) throws IOException {
         FileUtils.insert(new Student("hiall1",20,4.0));
         FileUtils.insert(new Student("hiall2",30,4.0));
+        FileUtils.get();
+        FileUtils.get();
 	}
 }
